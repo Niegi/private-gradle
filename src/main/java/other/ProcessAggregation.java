8f646ab4e08a5e6fdbccfc7931ebe1608e2c7844
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2021-05-07 13:03:09 +0200

    working solution

--- src/main/java/other/ProcessAggregation.java
+++ src/main/java/other/ProcessAggregation.java
@@ -1,7 +1,9 @@
 package other;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProcessAggregation {
 
@@ -21,11 +23,32 @@ public class ProcessAggregation {
         String name;
         long memory; // summarized memory of all processes
         List<Integer> pids; // list of process identifiers for all processes with the same name
+
+        @Override
+        public String toString() {
+            return "name: " + name +
+                    "\nmemory: " + memory +
+                    "\npids list: " + pids.toString();
+        }
     }
 
     static List<ProcessGroup> aggregate(List<Process> processes) {
-        //TODO: missing implementation
-        return Collections.emptyList();
+        Map<String, ProcessGroup> processGroups = new HashMap<>();
+        processes.forEach(p -> {
+                    if (!processGroups.containsKey(p.name)) {
+                        ProcessGroup processGroup = new ProcessGroup();
+                        processGroup.name = p.name;
+                        processGroup.memory = p.memory;
+                        processGroup.pids = new ArrayList<>();
+                        processGroup.pids.add(p.pid);
+                        processGroups.put(p.name, processGroup);
+                    } else {
+                        processGroups.get(p.name).memory += p.memory;
+                        processGroups.get(p.name).pids.add(p.pid);
+                    }
+                });
+        List<ProcessGroup> processGroupsAsList = new ArrayList<>(processGroups.values());
+        return processGroupsAsList;
     }
 
     public static void main(String[] args) {
@@ -34,6 +57,6 @@ public class ProcessAggregation {
                 new Process(2, "firefox.exe", 2000),
                 new Process(2, "chrome.exe", 3000));
 
-        System.out.println(aggregate(processes));
+        aggregate(processes).forEach(System.out::println);
     }
 }
