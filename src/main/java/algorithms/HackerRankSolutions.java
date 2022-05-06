d1af9e67e20c446556b18ebc4511ffb6562e9be9
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2022-05-06 20:04:57 +0200

    findMergeNode

--- src/main/java/algorithms/HackerRankSolutions.java
+++ src/main/java/algorithms/HackerRankSolutions.java
@@ -1003,6 +1003,24 @@ public class HackerRankSolutions {
     }
   }
 
+  // Complete the findMergeNode function below
+
+  public static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+    SinglyLinkedListNode current1 = head1;
+    SinglyLinkedListNode current2 = head2;
+    while (current1 != null && current2 != null) {
+      if (current1 == current2) {
+        return current1.data;
+      } else if (current2.next != null) {
+        current2 = current2.next;
+      } else {
+        current1 = current1.next;
+        current2 = head2;
+      }
+    }
+    return 0;
+  }
+
   /*
    * Complete the 'surfaceArea' function below.
    *
