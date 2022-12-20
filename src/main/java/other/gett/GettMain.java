c9374e4163190429cdb8ad2d2ba600ab5c0fe365
PNM386 <tomasz.niegowski@motorolasolutions.com>
2022-12-21 00:12:59 +0100

    gett interview

--- src/main/java/other/gett/GettMain.java
+++ src/main/java/other/gett/GettMain.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class GettMain {
 
   public static void main(String[] args) {
-    Mazda myMazda = new Mazda("red", "some manufactor", 4, "C30");
-    Kia myKia = new Kia("white", "other manufactor", 4, "Ceed");
+    Car myMazda = new Mazda("red", "some manufactor", 4, "C30");
+    Car myKia = new Kia("white", "other manufactor", 4, "Ceed");
     List<Car> cars = List.of(myMazda, myKia);
     cars.forEach(GettMain::printCarInfo);
   }
